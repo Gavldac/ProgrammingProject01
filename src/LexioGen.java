be214652010d98009d306be759794aba05535342
Edwin Casady <edwin.casady@gmail.com>
2025-02-07 13:21:09 -0700

    Implemented Lexicographic mutator

--- src/LexioGen.java
+++ src/LexioGen.java
@@ -1,67 +1,119 @@
-// This is my attempt at a Lexiographic Array Generator - Markus
-public class LexioGen
-{
+/**
+ * Class for generating lexicographic arrays
+ * Used for CS2430 Project 1 
+ * Group assignment
+ * 
+ * This is my attempt at a Lexiographic Array Generator - Markus
+ * 
+ * 
+ * @author Markus
+ * @author (revisions by) Edwin 
+ */
+public class LexioGen {
 
-    Integer size;   // how large is the array
-    Integer arrayNum;   // how many possible arrays are there
+    int size; // how large is the array
+    int arrayNum; // how many possible arrays are there
     Integer[][] arrays; // generated arrays
 
-    public LexioGen(Integer n) // constructor
+    public LexioGen(int n) // constructor
     {
         size = n;
         arrayNum = fact(n);
         arrays = new Integer[arrayNum][size];
+
+        generator();
     }
 
-    private static Integer fact(Integer n) // factorial function
+    private static int fact(Integer n) // factorial function
     {
-        return facttr(n, 1);
-    }
-    
-    /*
-    the generator function creates the first iteration and then utilizes the process and store functions
-    to create the rest of the arrays
-    */
-    private void generator(){
-    Integer[] tempVal = new Integer[size];
-    
-    // first iteration
-    for (Integer i = 0; i < size; i++){
-       arrays[0][i] = i;
-    }
-    
-    int index = 1;
-    
-    
-    }
-    
-    // This is the function where you would calculate the next lexiographic permutation.
-    // I will finish writing this later unless someone wants to pick up where I left off before I get back to it.
-    private boolean process(Integer [] temp){
-        /*
-        Search "Compute The Next Permutation of A Numeric Sequence - Case Analysis" on youtube to understand more about what the
-        function will do. Once the function reaches the end of the iterations it should return a negative boolean value to indicate the while loop
-        in the generator function to stop storing values.
-        */
-    }
-    
-    
-    // used to store next iteration
-    private void store(Integer[] j, int index){
-     for (int i = 0; i < size; i++){
-       arrays[index][i] = j[i];
-    }
-    }
-
-    private static Integer facttr(Integer n, Integer acc) // continuation of factorial function so it is tail recursive
-    {
-        if (n == 0)
-        {
-            return acc;
-        } else
-        {
-            return facttr(n - 1, acc * n);
+        int factorial = 1;
+        while (n > 0) {
+            factorial *= n--;
         }
+        return factorial;
+    }
+
+    /*
+     * the generator function creates the first iteration and then 
+     * increments through each array 
+     * and stores the next lexicographic permutation
+     */
+    private void generator() {
+        /*
+         * first generate values 0 to size-1 to the first array
+         * i.e. (arrays[0], {0,1,2,...,size-1} )
+         */
+        for (int i = 0; i < size; i++) {
+            arrays[0][i] = i;
+        }
+
+        /*
+         * next we generate the sequences of lexicographic permutations
+         * and store each permutation after arrays[0]
+         * i.e. arrays[1,2,3,...,arrayNum-1], [(next permutaion)]
+         */
+        Integer[] next = arrays[0].clone(); // pointers are so much fun
+        for (int i = 1; i < arrays.length; i++) {
+            next = nextPermutation(next); // pointers are so much fun
+            arrays[i] = next.clone(); // pointers are so much fun
+        }
+    }
+
+    /**
+     * Generates the next permutation of an array of integers
+     * according to the lexicographic order
+     * 
+     * @param Integer [] arrInts
+     * @return Interger[] next
+     */
+    private Integer[] nextPermutation(Integer[] arrInts) {
+        /*
+         * Using arrInts as a starting point, we need to create a new
+         * array to be the new/next permutation.
+         */
+        Integer[] next = arrInts.clone();
+
+        /*
+         * working right to left from the end of the array,
+         * find the first pair where arr[i]<arr[i+1]
+         */
+        int i = next.length - 2;
+        while (i >= 0 && next[i] >= next[i + 1])
+            i--;
+
+        /*
+         * working right to left from the end of the array,
+         * ind the first index j where arr[i]<arr[j]
+         */
+        int j = next.length - 1;
+        while (next[j] <= next[i])
+            j--;
+
+        /*
+         * swap arr[i] and arr[j]
+         */
+        swap(next, i, j);
+
+        /*
+         * reverse the order of elements after index i to the end of the array
+         */
+        i++;
+        j = next.length - 1;
+        while (i < j) {
+            swap(next, i++, j--);
+
+        }
+
+        /*
+         * next permutation is returned
+         */
+        return next;
+    }
+    // helper function for nextPermutation()
+    private void swap(Integer[] next, int i, int j) {
+        Integer tmp = next[i];
+        next[i] = next[j];
+        next[j] = tmp;
     }
 
 }
--- src/Project01.java
+++ src/Project01.java
@@ -3,80 +3,91 @@ import edu.princeton.cs.algs4.Quick;
  */
 public class Project01 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Need test client here");
 
-        Integer[] bigNums = { 1, 3, 5, 7, 43, 8, 76, 34, 45, 83, 90, 13, 16, 58, 53, 42, 89, 14, 61, 2 };
-        Integer[] smallNums = { 5, 4, 3, 2, 1 };
-        Integer[] orderedNums = { 1, 2, 3, 4, 5 };
+        LexioGen test1 = new LexioGen(5);
 
-        // reused variables for testing
-        Integer[] test;
-        int result;
+      for (int i = 0; i < test1.arrays.length; i++) {
+            for (int j = 0; j < test1.arrays[i].length; j++) {
+                System.out.print(test1.arrays[i][j] + " ");
+            }
+            System.out.println();
+      }
 
-        // Merge sort
-        System.out.println("Merge sort");
-        System.out.println("---------------------------------------------------");
 
-        test = bigNums.clone();
-        System.out.println("Comparisons: " + Merge.sort(test));
-        show(test);
+        // System.out.println("Need test client here");
 
-        test = smallNums.clone();
-        System.out.println("Comparisons: " + Merge.sort(test));
-        show(test);
+        // Integer[] bigNums = { 1, 3, 5, 7, 43, 8, 76, 34, 45, 83, 90, 13, 16, 58, 53, 42, 89, 14, 61, 2 };
+        // Integer[] smallNums = { 5, 4, 3, 2, 1 };
+        // Integer[] orderedNums = { 1, 2, 3, 4, 5 };
 
-        test = orderedNums.clone();
-        System.out.println("Comparisons: " + Merge.sort(test));
-        show(test);
-        System.out.println();
+        // // reused variables for testing
+        // Integer[] test;
+        // int result;
 
-        // Quick.sort(test);
-        System.out.println("Quick sort");
-        System.out.println("---------------------------------------------------");
+        // // Merge sort
+        // System.out.println("Merge sort");
+        // System.out.println("---------------------------------------------------");
 
-        test = bigNums.clone();
-        System.out.println("Comparisons: " + Quick.sort(test));
-        show(test);
+        // test = bigNums.clone();
+        // System.out.println("Comparisons: " + Merge.sort(test));
+        // show(test);
 
-        test = smallNums.clone();
-        System.out.println("Comparisons: " + Quick.sort(test));
-        show(test);
+        // test = smallNums.clone();
+        // System.out.println("Comparisons: " + Merge.sort(test));
+        // show(test);
 
-        test = orderedNums.clone();
-        System.out.println("Comparisons: " + Quick.sort(test));
-        show(test);
+        // test = orderedNums.clone();
+        // System.out.println("Comparisons: " + Merge.sort(test));
+        // show(test);
+        // System.out.println();
 
-        // Heap.sort(test);
-        System.out.println("Heap sort");
-        System.out.println("---------------------------------------------------");
+        // // Quick.sort(test);
+        // System.out.println("Quick sort");
+        // System.out.println("---------------------------------------------------");
 
-        test = bigNums.clone();
-        System.out.println("Comparisons: " + Heap.sort(test));
-        show(test);
+        // test = bigNums.clone();
+        // System.out.println("Comparisons: " + Quick.sort(test));
+        // show(test);
 
-        test = smallNums.clone();
-        System.out.println("Comparisons: " + Heap.sort(test));
-        show(test);
+        // test = smallNums.clone();
+        // System.out.println("Comparisons: " + Quick.sort(test));
+        // show(test);
+
+        // test = orderedNums.clone();
+        // System.out.println("Comparisons: " + Quick.sort(test));
+        // show(test);
+
+        // // Heap.sort(test);
+        // System.out.println("Heap sort");
+        // System.out.println("---------------------------------------------------");
+
+        // test = bigNums.clone();
+        // System.out.println("Comparisons: " + Heap.sort(test));
+        // show(test);
+
+        // test = smallNums.clone();
+        // System.out.println("Comparisons: " + Heap.sort(test));
+        // show(test);
  
-        test = orderedNums.clone();
-        System.out.println("Comparisons: " + Heap.sort(test));
-        show(test);       
+        // test = orderedNums.clone();
+        // System.out.println("Comparisons: " + Heap.sort(test));
+        // show(test);       
         
-        // Shaker.sort(args);
-        System.out.println("Shaker sort");
-        System.out.println("---------------------------------------------------");
+        // // Shaker.sort(args);
+        // System.out.println("Shaker sort");
+        // System.out.println("---------------------------------------------------");
         
-        test = bigNums.clone();
-        System.out.println("Comparisons: " + Shaker.sort(test));
-        show(test);
+        // test = bigNums.clone();
+        // System.out.println("Comparisons: " + Shaker.sort(test));
+        // show(test);
 
-        test = smallNums.clone();
-        System.out.println("Comparisons: " + Shaker.sort(test));
-        show(test);
+        // test = smallNums.clone();
+        // System.out.println("Comparisons: " + Shaker.sort(test));
+        // show(test);
 
-        test = orderedNums.clone();
-        System.out.println("Comparisons: " + Shaker.sort(test));
-        show(test);
+        // test = orderedNums.clone();
+        // System.out.println("Comparisons: " + Shaker.sort(test));
+        // show(test);
 
 
     }
