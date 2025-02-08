4093b690f7c49a1a9a06c0bba08c1bfbc45b410c
Gavldac <edwin.casady@gmail.com>
2025-02-07 23:28:39 -0700

    trimmed up TestDrive

--- src/Project01.java
+++ src/Project01.java
@@ -4,107 +4,22 @@ import edu.princeton.cs.algs4.Quick;
 public class Project01 {
     public static void main(String[] args) throws Exception {
 
-        LexioGen test1 = new LexioGen(5);
 
-      for (int i = 0; i < test1.arrays.length; i++) {
-            for (int j = 0; j < test1.arrays[i].length; j++) {
-                System.out.print(test1.arrays[i][j] + " ");
-            }
-            System.out.println();
-      }
+        String n4 = "Matlab Script/n4.csv";
+        String n6 = "Matlab Script/n6.csv";
+        String n8 = "Matlab Script/n8.csv";
 
+        TestDriver test1 = new TestDriver(4, n4);
 
-        // System.out.println("Need test client here");
+        TestDriver test2 = new TestDriver(6, n6);
 
-        // Integer[] bigNums = { 1, 3, 5, 7, 43, 8, 76, 34, 45, 83, 90, 13, 16, 58, 53, 42, 89, 14, 61, 2 };
-        // Integer[] smallNums = { 5, 4, 3, 2, 1 };
-        // Integer[] orderedNums = { 1, 2, 3, 4, 5 };
+        TestDriver test3 = new TestDriver(8, n8);
 
-        // // reused variables for testing
-        // Integer[] test;
-        // int result;
-
-        // // Merge sort
-        // System.out.println("Merge sort");
-        // System.out.println("---------------------------------------------------");
-
-        // test = bigNums.clone();
-        // System.out.println("Comparisons: " + Merge.sort(test));
-        // show(test);
-
-        // test = smallNums.clone();
-        // System.out.println("Comparisons: " + Merge.sort(test));
-        // show(test);
-
-        // test = orderedNums.clone();
-        // System.out.println("Comparisons: " + Merge.sort(test));
-        // show(test);
-        // System.out.println();
-
-        // // Quick.sort(test);
-        // System.out.println("Quick sort");
-        // System.out.println("---------------------------------------------------");
-
-        // test = bigNums.clone();
-        // System.out.println("Comparisons: " + Quick.sort(test));
-        // show(test);
-
-        // test = smallNums.clone();
-        // System.out.println("Comparisons: " + Quick.sort(test));
-        // show(test);
-
-        // test = orderedNums.clone();
-        // System.out.println("Comparisons: " + Quick.sort(test));
-        // show(test);
-
-        // // Heap.sort(test);
-        // System.out.println("Heap sort");
-        // System.out.println("---------------------------------------------------");
-
-        // test = bigNums.clone();
-        // System.out.println("Comparisons: " + Heap.sort(test));
-        // show(test);
-
-        // test = smallNums.clone();
-        // System.out.println("Comparisons: " + Heap.sort(test));
-        // show(test);
- 
-        // test = orderedNums.clone();
-        // System.out.println("Comparisons: " + Heap.sort(test));
-        // show(test);       
-        
-        // // Shaker.sort(args);
-        // System.out.println("Shaker sort");
-        // System.out.println("---------------------------------------------------");
-        
-        // test = bigNums.clone();
-        // System.out.println("Comparisons: " + Shaker.sort(test));
-        // show(test);
-
-        // test = smallNums.clone();
-        // System.out.println("Comparisons: " + Shaker.sort(test));
-        // show(test);
-
-        // test = orderedNums.clone();
-        // System.out.println("Comparisons: " + Shaker.sort(test));
-        // show(test);
-
+       
 
     }
 
-    private static void show(Integer[] a) {
-        int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            count++;
-            if (count % 10 > 0)
-                System.out.print(a[i] + " ");
-            else {
-                System.out.println(a[i]);
-            }
-        }
-        System.out.println();
-    }
-
+   
 }
 
 // hello
