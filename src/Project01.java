effaf297498ca820a1b66cca6db0dcd969d9fefd
Gavldac <75696834+Gavldac@users.noreply.github.com>
2025-02-10 14:43:43 -0700

    Update Project01.java doc comment updates

--- src/Project01.java
+++ src/Project01.java
@@ -1,5 +1,9 @@
-/* import edu.princeton.cs.algs4.Merge;
-import edu.princeton.cs.algs4.Quick;
+/**
+ * Class for running the test driver Used for CS2430 Project 1 Group assignment
+ * TestDriver is used to run the tests on the sorting algorithms with arguements  
+ * for array size and the file path to write the results to.
+ * 
+ * @author Edwin Casady
  */
 public class Project01 {
     public static void main(String[] args) throws Exception {
